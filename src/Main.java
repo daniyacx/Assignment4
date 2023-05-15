ff42b8a625ec396f1c051ac1b3fb8ba69ac448e5
aubak <aubakirovadania@gmail.com>
2023-05-15 13:27:11 +0600

    testing myHashTable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyHashTable<String, Integer> hashTable = new MyHashTable<>(10);
+        hashTable.put("daniya", 1);
+        hashTable.put("alua", 2);
+        hashTable.put("elya", 3);
+        System.out.println(hashTable.get("alua"));
+        System.out.println(hashTable.contains(3));
+        System.out.println(hashTable.contains(4));
+        System.out.println(hashTable.getKey(2));
+        System.out.println(hashTable.remove("elya"));
+        System.out.println(hashTable.get("elya"));
+        int[] bucketSizes = hashTable.getBucketSizes();
+        for (int i = 0; i < bucketSizes.length; i++) {
+            System.out.println("Bucket " + i + " has " + bucketSizes[i] + " elements.");
+        }
     }
 }
