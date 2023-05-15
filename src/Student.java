624bc329e5c414b4f69d19f8cd5f713071cf3c27
aubak <aubakirovadania@gmail.com>
2023-05-15 16:48:56 +0600

    comments and documentation for Students

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,18 @@
 import java.util.Objects;
 
 public class Student {
+    // The student's name
     private String name;
+    // The student's group number
     private int group;
+    // The student's grade point average
     private float gpa;
+    /**
+     constructs a new Student object with the specified name, group number, and grade point average
+     @param name the student's name
+     @param group the student's group number
+     @param gpa the student's grade point average
+     */
     public Student(String name, int group, float gpa){
         this.group = group;
         this.name = name;
